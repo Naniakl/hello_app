383c8af2f679c566850e20e4c7fa24d166814199
nani <you@example.com>
2018-07-06 00:29:23 +0530

    generate views and create separate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+  get 'static_pages/index'
+  get 'static_pages/help'
+  get 'static_pages/about'
   resources :microposts
   resources :users
-  root 'users#index'
 end
